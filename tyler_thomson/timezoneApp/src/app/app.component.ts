307af3421de400cad4b347fec6c140dacc344550
Tyler Thomson <tylerthomson@protonmail.com>
2017-07-18 12:26:24 -0500

    Added unfinished timezoneApp assignment

---
+++ tyler_thomson/timezoneApp/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myDate: any = new Date();
+
+  currentTime(timezone){
+    if(timezone == "PST"){
+      this.myDate = this.myDate.setHours(this.myDate.getHours()-2);
+    }
+    else if(timezone == "MST"){
+      this.myDate = this.myDate.setHours(this.myDate.getHours()-1);
+    }
+    else if(timezone == "CST"){
+      this.myDate = new Date;
+    }
+    else if(timezone == "EST"){
+      this.myDate = this.myDate.setHours(this.myDate.getHours()+1);
+    }
+  }
+
+  reset(){
+    this.myDate = new Date();
+  }
+}
